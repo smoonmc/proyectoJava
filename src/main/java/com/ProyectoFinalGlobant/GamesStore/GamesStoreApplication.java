17df93529634fbc11a7624db9cefd68b6f0362e7
mcreyes <mariacreyes@gmail.com>
2021-07-12 10:52:44 -0400

    Proyecto Base 1

---
+++ src/main/java/com/ProyectoFinalGlobant/GamesStore/GamesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinalGlobant.GamesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesStoreApplication.class, args);
+	}
+
+}
